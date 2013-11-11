9b541f1bbf7f31c55d7009738a49658ce489ba10
chuzheng <chuzheng@gmail.com>
2013-11-11 20:15:52 +0800

    begin buff

--- test/buffer.js
+++ test/buffer.js
@@ -1,3 +1,24 @@
 var should = require('should');
 
+describe('Buffer', function () {
+    describe('#new Buffer(size)', function() {
+        it('should be an new buffer with (size) byte', function() {
+            var buf = new Buffer(1024)
+            buf.should.be.an.instanceof(Buffer)
+        })
+    })
 
+    describe('#new Buffer(array)', function() {
+        it('should be an new buffer with (array) byte', function() {
+            var buf = new Buffer([0, 1, 255])
+            should.strictEqual(buf[2], 255)
+        })
+    })
+
+    describe('new Buffer(str, [encoding])', function() {
+        it('should be an new buffer with str with [encoding] default to "utf-8" ', function() {
+            var buf = new Buffer('深圳')
+            buf.should.be.an.instanceof(Buffer)
+        })
+    })
+})
