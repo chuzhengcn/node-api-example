61a799b5b010e8101e082340d1cb4f455dc41888
chuzheng <chuzhengcn@gmail.com>
2013-11-14 15:08:56 +0800

    add some buffer example

--- test/buffer.js
+++ test/buffer.js
@@ -64,4 +64,86 @@ describe('Buffer', function () {
             buf.toString().should.be.exactly('breaking bad');
         })
     })
+
+    describe('Buffer.isBuffer(obj)', function() {
+        it('should be true if obj is a Buffer object', function() {
+            var buf = new Buffer('breaking bad');
+
+            Buffer.isBuffer(buf).should.be.true;
+
+        })
+    })
+
+    describe('Class Method: Buffer.byteLength(string, [encoding])', function() {
+        it('should return actual byte length of a string', function() {
+            var byte_length = Buffer.byteLength('楚正')
+
+            byte_length.should.exactly(6);
+
+        })
+    })
+
+    describe('Class Method: Buffer.concat(list, [totalLength])', function() {
+        var buf1 = new Buffer('Pinkman'),
+            buf2 = new Buffer('breakingbad'); 
+
+        it('should return Pinkman, If the list has exactly one item', function() {
+            Buffer.concat([buf1]).should.exactly(buf1)     
+        })
+
+        it('should return new Buffer inclued breaking bad Pinkman', function() {
+            Buffer.concat([buf2,buf1], 18).toString().should.exactly('breakingbadPinkman')     
+        })
+
+    })
+
+    describe('buf.length', function() {
+        var buf = new Buffer(256)
+
+        it('should always equal amount of memory allocated for the buffer object', function() {
+            buf.length.should.exactly(256)   
+        })
+
+    })
+
+    describe('buf.copy(targetBuffer, [targetStart], [sourceStart], [sourceEnd])', function() {
+        var buf     = new Buffer('Pinkman'),
+            target  = new Buffer(7); 
+
+        it('target should be Pinkman', function() {
+            buf.copy(target)
+            target.toString().should.exactly('Pinkman')   
+        })
+    })
+
+    describe('buf.slice([start], [end])', function() {
+        it('should be !bc which come from abc', function() {
+            var buf1 = new Buffer(26);
+
+            for (var i = 0 ; i < 26 ; i++) {
+                buf1[i] = i + 97; // 97 is ASCII a
+            }
+
+            var buf2 = buf1.slice(0, 3);
+            buf1[0] = 33;
+            buf2.toString('ascii', 0, buf2.length).should.exactly('!bc');
+        })
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
