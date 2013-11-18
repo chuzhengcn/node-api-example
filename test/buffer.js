1e49b43886ada9a465b4b18689a19d0fda22599b
chuzheng <chuzheng@gmail.com>
2013-11-18 18:29:21 +0800

    buffer

--- test/buffer.js
+++ test/buffer.js
@@ -189,4 +189,161 @@ describe('Buffer', function () {
             buf.readInt8(1).should.exactly(-1)
         })
     })
+
+    describe('buf.readInt16LE(offset, [noAssert]), buf.readInt16BE(offset, [noAssert])', function() {
+        var buf = new Buffer(4);
+
+        buf[0] = 0xff;
+        buf[1] = 0xff;
+        buf[2] = 0x23;
+        buf[3] = 0x42;
+
+        it('should return a negtive number', function() {
+            buf.readInt16BE(1).should.be.below(0)
+        })
+
+        it('should return a positive number', function() {
+            buf.readInt16LE(1).should.be.above(0)
+        })
+    })
+
+    describe('buf.readInt32LE(offset, [noAssert]), buf.readInt32BE(offset, [noAssert])', function() {
+        var buf = new Buffer(4);
+
+        buf[0] = 0xff;
+        buf[1] = 0xff;
+        buf[2] = 0x23;
+        buf[3] = 0x42;
+
+        it('should return a negtive number', function() {
+            buf.readInt32BE(0).should.be.below(0)
+        })
+
+        it('should return a positive number', function() {
+            buf.readInt32LE(0).should.be.above(0)
+        })
+    })
+
+    describe('buf.readFloatLE(offset, [noAssert]), buf.readFloatBE(offset, [noAssert])', function() {
+        var buf = new Buffer(4);
+
+        buf[0] = 0xf0;
+        buf[1] = 0x00;
+        buf[2] = 0x80;
+        buf[3] = 0x00;
+
+        // 第一位表示正负，后八位表示指数，后23位表示精度
+
+        it('should return a negtive float', function() {
+            buf.readFloatBE(0).should.below(0)
+            buf.readFloatBE(0).toString().indexOf('.').should.above(-1)
+        })
+
+        it('should return a positive float', function() {
+            buf.readFloatLE(0).should.be.above(0)
+            buf.readFloatLE(0).toString().indexOf('.').should.above(-1)
+        })
+    })
+
+    describe('buf.readDoubleLE(offset, [noAssert]), buf.readDoubleBE(offset, [noAssert])', function() {
+        var buf = new Buffer(8);
+
+        buf[0] = 0xf5;
+        buf[1] = 0x55;
+        buf[2] = 0x55;
+        buf[3] = 0x55;
+        buf[4] = 0x55;
+        buf[5] = 0x55;
+        buf[6] = 0xd5;
+        buf[7] = 0x3f;
+
+        // 第一位表示正负， 后面11位表示次方数， 最后52位表示精度
+
+        it('should return a negtive float', function() {
+            buf.readDoubleBE(0).should.below(0)
+            buf.readDoubleBE(0).toString().indexOf('.').should.above(-1)
+        })
+
+        it('should return a positive float', function() {
+            buf.readDoubleLE(0).should.be.above(0)
+            buf.readDoubleLE(0).toString().indexOf('.').should.above(-1)
+        })
+    })
+
+    describe('buf.writeUInt8(value, offset, [noAssert])', function() {
+        var buf = new Buffer(4);
+        buf.writeUInt8(0x3, 0);
+        buf.writeUInt8(0x4, 1);
+        buf.writeUInt8(0x23, 2);
+        buf.writeUInt8(0x42, 3);
+
+        it('should return a Buffer', function() {
+            buf.toJSON()[1].should.exactly(4)
+        })
+    })
+
+    describe('buf.writeUInt16LE(value, offset, [noAssert]), buf.writeUInt16BE(value, offset, [noAssert])', function() {
+        it('should return a hex', function() {
+            var buf = new Buffer(4);
+            buf.writeUInt16BE(0xdead, 0);
+            buf.writeUInt16BE(0xbeef, 2);
+
+            buf.toJSON()[0].toString(16).should.exactly('de')
+            buf.toJSON()[1].toString(16).should.exactly('ad')
+        })
+
+        it('should return a hex', function() {
+            var buf = new Buffer(4);
+            buf.writeUInt16LE(0xdead, 0);
+            buf.writeUInt16LE(0xbeef, 2);
+            
+            buf.toJSON()[0].toString(16).should.exactly('ad')
+            buf.toJSON()[1].toString(16).should.exactly('de')
+        })
+    })
+
+    describe('buf.writeUInt32LE(value, offset, [noAssert]), buf.writeUInt32BE(value, offset, [noAssert])', function() {
+        it('should return a hex', function() {
+            var buf = new Buffer(4);
+            buf.writeUInt32BE(0xfeedface, 0);
+
+            buf.toJSON()[0].toString(16).should.exactly('fe')
+            buf.toJSON()[3].toString(16).should.exactly('ce')
+        })
+
+        it('should return a hex', function() {
+            var buf = new Buffer(4);
+            buf.writeUInt32LE(0xfeedface, 0);
+            
+            buf.toJSON()[0].toString(16).should.exactly('ce')
+            buf.toJSON()[1].toString(16).should.exactly('fa')
+        })
+    })
+
+    describe('buf.writeFloatLE(value, offset, [noAssert]), buf.writeFloatBE(value, offset, [noAssert])', function() {
+        it('should write float success', function() {
+            var buf = new Buffer(4);
+            buf.writeFloatBE(0xcafebabe, 0);
+
+            buf.toJSON()[0].toString(16).should.exactly('4f')
+        })
+    })
+
+    describe('buf.writeDoubleLE(value, offset, [noAssert]), buf.writeDoubleBE(value, offset, [noAssert])', function() {
+        it('should write double success', function() {
+            var buf = new Buffer(8);
+            buf.writeDoubleBE(0xdeadbeefcafebabe, 0);
+
+            buf.toJSON()[0].toString(16).should.exactly('43')
+        })
+    })
+
+    describe('buf.fill(value, [offset], [end])', function() {
+        it('should return all ccccc', function() {
+            var buf = new Buffer(5);
+            buf.fill('c')
+
+            buf.toString().should.exactly('ccccc')
+        })
+    })
 })
